096ca2edc3ad51f78dec9ee8d128186ef1eacb51
ravidasari00 <123715151+ravidasari00@users.noreply.github.com>
2023-12-14 01:02:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def index():
         # Close the connection
         connection.close()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
